cdf3296490f35d18474cbc89bd52d2279ee5d8bd
luyangong <luyangong@qq.com>
2022-08-14 23:59:16 +0800

    fix code

--- src/lox.py
+++ src/lox.py
@@ -1,18 +1,26 @@
 import sys
 
 def main():
-    if len(sys.args) > 1:
+    if len(sys.argv) > 2:
         print("Usage: plox [script]")
         exit(64)
-    elif len(sys.args) == 1:
-        run_file(sys.args[0])
+    elif len(sys.argv) == 2:
+        run_file(sys.argv[0])
     else:
         run_prompt()
 
 def run_file(filename: str):
-    pass
+    with open(filename) as f:
+        run(f.read())
 
 def run_prompt():
+    while True:
+        line = input("> ")
+        if not line:
+            break
+        run(line)
+
+def run(string: str):
     pass
 
 if __name__ == "__main__":
